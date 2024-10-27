72da3c0f29237b56cec0542990fccf50d1d9a0c4
joaomacedocb <joaomacedocb@gmail.com>
2024-10-27 11:31:08 -0300

    Adicionado model escritorio e realizada a migracao

---
+++ escritorio/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+class Escritorio(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=200)
+    cep = models.CharField(
+        max_length=9, 
+        validators=[
+            RegexValidator(regex=r'^\d{5}-\d{3}$', message="CEP deve estar no formato 99999-999.")
+        ],
+    )
+    cnpj = models.CharField(max_length=14, blank=True, null=True)
+    endereco = models.CharField(max_length=100, blank=True, null=True)
+    numero = models.CharField(max_length=10, blank=True, null=True)
+    cidade = models.CharField(max_length=50, blank=True, null=True)
+    uf = models.CharField(max_length=2, blank=True, null=True)
+    responsavel_tecnico = models.CharField(max_length=50)
+    telefone = models.CharField(
+        max_length=15, blank=True, null=True,
+        validators=[
+            RegexValidator(
+                regex=r'^\(?\d{2}\)? ?9?\d{4}-\d{4}$',
+                message="O número de telefone deve estar no formato (99) 99999-9999 ou (99) 9999-9999."
+            )
+        ]
+    )
--- main.py
+++
@@ -1,9 +0,0 @@
-valor1 = 14
-valor2 = 4
-nome = "João"
-
-soma = valor1 + valor2
-
-print(f"A soma é {soma}.")
-
-print(f"Olá, {nome}. A soma é {soma}.")
