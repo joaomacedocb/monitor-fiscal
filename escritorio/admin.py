de472e7ef851370f9c280912705b23f662963fd1
joaomacedocb <joaomacedocb@gmail.com>
2024-10-27 11:49:08 -0300

    Registrado model escritorio no modulo de administracao

--- escritorio/admin.py
+++ escritorio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from escritorio.models import Escritorio
 
-# Register your models here.
+class EscritorioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'cnpj', 'cep', 'endereco', 'numero', 'cidade', 'uf', 'responsavel_tecnico', 'telefone')
+    search_fields = ('id','cnpj','nome')
+
+admin.site.register(Escritorio, EscritorioAdmin)
--- escritorio/models.py
+++ escritorio/models.py
@@ -4,13 +4,13 @@ from django.core.validators import RegexValidator
 class Escritorio(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200)
+    cnpj = models.CharField(max_length=14, blank=True, null=True)
     cep = models.CharField(
         max_length=9, 
         validators=[
             RegexValidator(regex=r'^\d{5}-\d{3}$', message="CEP deve estar no formato 99999-999.")
         ],
     )
-    cnpj = models.CharField(max_length=14, blank=True, null=True)
     endereco = models.CharField(max_length=100, blank=True, null=True)
     numero = models.CharField(max_length=10, blank=True, null=True)
     cidade = models.CharField(max_length=50, blank=True, null=True)
