391fec7d9149ba4be4378f977e061a351965acde
joaomacedocb <joaomacedocb@gmail.com>
2024-11-03 14:04:57 -0300

    Criacao da estrutura para clientes

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from clientes.views import clientes_view
 from escritorio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('escritorio/<int:id>/', views.escritorio_detalhes, name='escritorio_detalhes'),
+    path('clientes/', clientes_view, name='clientes'),
+ #   path('novo_cliente/', novo_cliente_view, name='novo_cliente'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from clientes.models import Cliente
 
-# Create your views here.
+def clientes_view(request):
+    clientes = Cliente.objects.all().order_by('nome_fantasia')
+    search = request.GET.get('search')
+    
+    if search:
+        clientes = clientes.filter(nome_fantasia__icontains = search)
+        
+    return render(request, 'clientes.html', {'clientes': clientes})
