a90d168ec494a91335253e8aaf9acc2d2e0dafd3
joaomacedocb <joaomacedocb@gmail.com>
2024-11-05 23:41:39 -0300

    Adicao de render na view e busca de informacoes na base

--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from clientes.models import Cliente
+from clientes.forms import ClienteForm
 
 def clientes_view(request):
     clientes = Cliente.objects.all().order_by('nome_fantasia')
@@ -9,3 +10,9 @@ def clientes_view(request):
         clientes = clientes.filter(nome_fantasia__icontains = search)
         
     return render(request, 'clientes.html', {'clientes': clientes})
+
+
+def novo_cliente_view(request):
+    
+    novo_cliente_form = ClienteForm()
+    return render(request, 'novo_cliente.html', { 'novo_cliente_form': novo_cliente_form })
