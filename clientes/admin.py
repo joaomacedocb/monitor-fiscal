b9d2d4bfeb058f45ac1b36839e516f83cdbac04a
joaomacedocb <joaomacedocb@gmail.com>
2024-11-05 23:40:09 -0300

    Adicao da entidade de regimes fiscais

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from clientes.models import Cliente
+from clientes.models import Cliente, RegimeFiscal
+
+class RegimeFiscalAdm(admin.ModelAdmin):
+    list_display = ('id','nome')
+    search_fields = ('id','nome')
 
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('id', 'cnpj', 'nome_fantasia', 'razao_social', 'email', 'telefone','regime_fiscal', 'ultima_atualizacao', 'status')
     search_fields = ('id','cnpj', 'nome_fantasia', 'razao_social','email')
 
 admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(RegimeFiscal, RegimeFiscalAdm)
