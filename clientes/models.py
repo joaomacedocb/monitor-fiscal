5e72586609831d6785e2adc6d22e3df1b04ef0ee
joaomacedocb <joaomacedocb@gmail.com>
2024-11-05 23:40:50 -0300

    Adicao de meta para regime fiscal

--- clientes/models.py
+++ clientes/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
+class RegimeFiscal(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name = "Regime Fiscal"
+        verbose_name_plural = "Regimes Fiscais"
+
 class Cliente(models.Model):
     id = models.AutoField(primary_key=True)
     nome_fantasia = models.CharField(max_length=200)
@@ -8,7 +19,7 @@ class Cliente(models.Model):
     cnpj = models.CharField(max_length=14, blank=True, null=True)
     responsavel_tecnico = models.CharField(max_length=50)
     email = models.CharField(max_length=200, blank=True, null=True)
-    regime_fiscal = models.CharField(max_length=20, blank= True, null= True)
+    regime_fiscal = models.ForeignKey(RegimeFiscal, on_delete=models.PROTECT, related_name= 'regime_fiscal')
     ultima_atualizacao = models.DateTimeField(blank=True, null=True)
     status = models.CharField(max_length=20, blank= True, null= True)
     telefone = models.CharField(
