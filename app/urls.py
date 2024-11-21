3a2bdffe8ada88667e3ea6400396761ba903e54f
joaomacedocb <joaomacedocb@gmail.com>
2024-11-20 23:58:09 -0300

    Adicionado recurso para criar usuário

--- app/urls.py
+++ app/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from clientes.views import ClienteDetailView, ClienteUpdateView, ClienteDeleteView, clientes_view, novo_cliente_view, atualizar_clientes_view
-from escritorio import views
+from escritorio.views import escritorio_detalhes
+from contas.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('escritorio/<int:id>/', views.escritorio_detalhes, name='escritorio_detalhes'),
+    path('escritorio/<int:id>/', escritorio_detalhes, name='escritorio_detalhes'),
     path('clientes/', clientes_view, name='clientes'),
     path('novo_cliente/', novo_cliente_view, name='novo_cliente'),
     path('atualizar-clientes/', atualizar_clientes_view, name='atualizar_clientes'),
     path('cliente/<int:pk>/', ClienteDetailView.as_view(), name = 'cliente_detalhe'),
     path('cliente/<int:pk>/update/', ClienteUpdateView.as_view(), name = 'cliente_update'),
     path('cliente/<int:pk>/delete/', ClienteDeleteView.as_view(), name = 'cliente_delete'),
+    path('registro/', register_view, name='register_view'),
 ]
--- contas/views.py
+++ contas/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('clientes')
+    elif request.method == 'GET':
+        user_form = UserCreationForm()
+    return render(request, 'registro.html', {
+            'user_form': user_form
+        })
