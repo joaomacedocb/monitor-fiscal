810cb87787629df1bf2950f3ef9c28b5abbaaaaa
joaomacedocb <joaomacedocb@gmail.com>
2024-11-21 23:46:17 -0300

    Adicionado versao 1 do sistema de login e registro

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from clientes.views import ClienteDetailView, ClienteUpdateView, ClienteDeleteView, clientes_view, novo_cliente_view, atualizar_clientes_view
 from escritorio.views import escritorio_detalhes
-from contas.views import register_view
+from contas.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +14,6 @@ urlpatterns = [
     path('cliente/<int:pk>/update/', ClienteUpdateView.as_view(), name = 'cliente_update'),
     path('cliente/<int:pk>/delete/', ClienteDeleteView.as_view(), name = 'cliente_delete'),
     path('registro/', register_view, name='register_view'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
