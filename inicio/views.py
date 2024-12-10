3529620988db9404b707e83ab367f8fd5157b7ce
joaomacedocb <joaomacedocb@gmail.com>
2024-12-09 23:11:14 -0300

    Adicionado filtro para aparecer apenas o historico de consultas do usuario logado

--- inicio/views.py
+++ inicio/views.py
@@ -5,18 +5,21 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def inicio_view(request):
-    total_consultas = ConsultaHistorico.objects.count()
-    total_exclusoes = ConsultaHistorico.objects.filter(data_exclusao__isnull=False).count()
-    consultas_list = ConsultaHistorico.objects.all().order_by('-data_consulta')
+    escritorio = request.user.escritorio
+    
+    consultas_list = ConsultaHistorico.objects.filter(escritorio=escritorio).order_by('-data_consulta')
+    
+    total_consultas = consultas_list.count()
+    total_exclusoes = consultas_list.filter(data_exclusao__isnull=False).count()
     
     paginator = Paginator(consultas_list, 10)
     page_number = request.GET.get('page')
-    consultas = paginator.get_page(page_number) 
+    consultas = paginator.get_page(page_number)
     
     context = {
         'total_consultas': total_consultas,
         'total_exclusoes': total_exclusoes,
         'consultas': consultas,
     }
+    
     return render(request, 'inicio.html', context)
-
