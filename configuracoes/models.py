169ffc5655e9ef4d8bec83b5bcbff9c9d3e7bff5
joaomacedocb <joaomacedocb@gmail.com>
2024-12-26 00:57:59 -0300

    Adiciona campo para controlar se envia notificacao por companhia

--- configuracoes/models.py
+++ configuracoes/models.py
@@ -5,4 +5,5 @@ from escritorio.models import Escritorio
 class Configuracoes(models.Model):
     cor_principal = models.TextField(null=True, blank=True)
     empresa_config = models.ForeignKey(Escritorio, on_delete=models.CASCADE)
-    email_notificacao = models.EmailField(max_length=100)
+    email_notificacao = models.EmailField(max_length=100, default='email@email.com')
+    envia_notificacao = models.BooleanField(default=False)
