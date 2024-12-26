b119d8a00cdb5e5abe94f9010643575bc02182d2
joaomacedocb <joaomacedocb@gmail.com>
2024-12-26 00:52:17 -0300

    Adicionado o campo de e-mail notificacao na model de configuracoes

--- configuracoes/models.py
+++ configuracoes/models.py
@@ -5,3 +5,4 @@ from escritorio.models import Escritorio
 class Configuracoes(models.Model):
     cor_principal = models.TextField(null=True, blank=True)
     empresa_config = models.ForeignKey(Escritorio, on_delete=models.CASCADE)
+    email_notificacao = models.EmailField(max_length=100)
