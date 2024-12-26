c26c8f0dd578c2759ec1710ec6558224995e6764
joaomacedocb <joaomacedocb@gmail.com>
2024-12-26 00:37:46 -0300

    Atualiza model de configuracoes

--- configuracoes/models.py
+++ configuracoes/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
+from escritorio.models import Escritorio
+
 class Configuracoes(models.Model):
-    cores = models.TextField(null=True, blank=True)
+    cor_principal = models.TextField(null=True, blank=True)
+    empresa_config = models.ForeignKey(Escritorio, on_delete=models.CASCADE)
