48bcf9a02a8a99defed91295d5f8b1a832f88599
joaomacedocb <joaomacedocb@gmail.com>
2025-01-13 23:21:44 -0300

    Atualizando settings para usar variaveis

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,13 +96,14 @@ AUTH_USER_MODEL = 'contas.CustomUser'
 #  }
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'fiscoalertdb',
-       'PASSWORD': 'tr@nsparens1a2025',
-       'HOST': 'localhost',
-       'PORT': '5432'
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME',),
+        'USER': config('DB_USER',),
+        'PASSWORD': config('DB_PASSWORD',),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
 }
 
 
